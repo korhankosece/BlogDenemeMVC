4517da3ab927d9a65d3c4704c935a38ca45538cc
Metin Korhan Kösece <korhankosece@gmail.com>
2022-01-13 13:19:52 +0300

    first commit

---
+++ BlogProject/Models/ORM/Author.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace BlogProject.Models.ORM
+{
+    public class Author : BaseEntity
+    {
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string UserName { get; set; }
+        public DateTime BirthDate { get; set; }
+        public bool Gender { get; set; }
+    }
+}
---
+++ BlogProject/Models/ORM/Blog.cs
@@ -0,0 +1,12 @@
+﻿
+namespace BlogProject.Models.ORM
+{
+    public class Blog : BaseEntity
+    {
+        public string Title { get; set; }
+        public string Subtitle { get; set; }
+        public string Text { get; set; }
+        public string Image { get; set; }
+        public int AuthorID { get; set; }
+    }
+}
