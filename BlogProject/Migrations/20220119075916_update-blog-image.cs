df7873d1024970d90b086055ae89953db13d1362
erenmestan <ermestan@gmail.com>
2022-01-19 11:00:07 +0300

    update foreignkey blog image

---
+++ BlogProject/Migrations/20220119075916_update-blog-image.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace BlogProject.Migrations
+{
+    public partial class updateblogimage : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "IX_BlogImages_BlogId",
+                table: "BlogImages",
+                column: "BlogId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_BlogImages_Blogs_BlogId",
+                table: "BlogImages",
+                column: "BlogId",
+                principalTable: "Blogs",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_BlogImages_Blogs_BlogId",
+                table: "BlogImages");
+
+            migrationBuilder.DropIndex(
+                name: "IX_BlogImages_BlogId",
+                table: "BlogImages");
+        }
+    }
+}
