010e5c262fbd199106afe6ea7aa602f934a16352
erenmestan <ermestan@gmail.com>
2022-01-24 21:50:24 +0300

    AdminUserController and Menu updates

--- BlogProject/Controllers/AdminUserController.cs
+++ BlogProject/Controllers/AdminUserController.cs
@@ -1,16 +1,93 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using BlogProject.Models.ORM;
+using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;
+using BlogProject.Models.VM;
 
 namespace BlogProject.Controllers
 {
     public class AdminUserController : Controller
     {
-        //public IActionResult Index()
-        //{
-        //    return View();
-        //}
+        BlogProjectContext _context;
+
+        public AdminUserController()
+        {
+            _context = new BlogProjectContext();
+        }
+
+        public IActionResult Index()
+        {
+            List<AdminUserVM> model = _context.AdminUsers.Where(p => p.IsDeleted == false).Select(q => new AdminUserVM()
+            {
+                Id = q.Id,
+                Email = q.Email,
+                Password = q.Password,
+                LastLoginDate = q.LastLoginDate
+            }).ToList();
+            return View(model);
+        }
+
+        public IActionResult Add()
+        {
+
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Add(AdminUserVM adminUserVm)
+        {
+            if (ModelState.IsValid)
+            {
+                AdminUser adminUser = new AdminUser();
+                adminUser.Email = adminUserVm.Email;
+                adminUser.Password = adminUserVm.Password;
+
+                _context.AdminUsers.Add(adminUser);
+                _context.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View();
+
+        }
+
+        public IActionResult Edit(int id)
+        {
+            AdminUser adminUser = _context.AdminUsers.FirstOrDefault(p => p.Id == id);
+            AdminUserVM adminUserVm = new AdminUserVM();
+            adminUserVm.Id = adminUser.Id;
+            adminUserVm.Email = adminUser.Email;
+            adminUserVm.Password = adminUser.Password;
+            adminUserVm.ConfirmPassword = adminUser.Password;
+
+            return View(adminUserVm);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(AdminUserVM adminUserVm)
+        {
+            if (ModelState.IsValid)
+            {
+                AdminUser adminUser = _context.AdminUsers.FirstOrDefault(p => p.Id == adminUserVm.Id);
+                adminUser.Id = adminUserVm.Id;
+                adminUser.Email = adminUserVm.Email;
+                adminUser.Password = adminUserVm.Password;
+                _context.AdminUsers.Update(adminUser);
+                _context.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(adminUserVm);
+        }
+
+        public IActionResult Delete(int Id)
+        {
+            AdminUser adminUser = _context.AdminUsers.FirstOrDefault(p => p.Id == Id);
+            adminUser.IsDeleted = true;
+            _context.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
     }
 }
--- BlogProject/Models/VM/AdminUserVM.cs
+++ BlogProject/Models/VM/AdminUserVM.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,8 +9,14 @@ namespace BlogProject.Models.VM
     public class AdminUserVM
     {
         public int Id { get; set; }
+        [Required]
         public string Email { get; set; }
+
+        [Required]
         public string Password { get; set; }
+
+        [Compare("Password",ErrorMessage = "Confirm password does not match!")]
+        public string ConfirmPassword { get; set; }
         public DateTime LastLoginDate { get; set; }
     }
 }
